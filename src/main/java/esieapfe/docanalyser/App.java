7da0ffcabef865ec1217486f95947dafb89fbeb3
bahriAY <ay@et.esiea.fr>
2017-12-11 12:58:03 +0100

    first

---
+++ src/main/java/esieapfe/docanalyser/App.java
@@ -0,0 +1,13 @@
+package esieapfe.docanalyser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
